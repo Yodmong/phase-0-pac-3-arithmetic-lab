bca62592848c6983092b1c4cc57ebce8298f65db
tidaraty <frankpook1978@gmail.com>
2023-04-03 16:07:36 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,36 @@
+// we'll fill this in shortl
+function add() {
+    return "something";
+  }
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+} 
+  
+function divide(a,b) {
+    return a/b;
+}
+
+function add(a,b) {
+    return a + b;
+}
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+    return (parseInt(n, "10"));
+}
+
+function preserveDecimal(n){
+    return (parseFloat(n, "10"));
+}
